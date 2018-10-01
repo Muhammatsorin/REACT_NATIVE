57a3e0d5084254fcc0cfa1b82ed1b93da5b5fc91
Muhammatsorin <sorin.13122540@gmail.com>
2018-10-02 03:30:51 +0700

    edit style 2

--- components/WeatherScreen.js
+++ components/WeatherScreen.js
@@ -1,23 +1,53 @@
+// import React from 'react';
+// import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+// import Weather from './Weather';
+
+// export default class WeatherScreen extends React.Component {
+//     static navigationOptions = ({ navigation }) => {
+//         return {
+//             headerTitle: (<Text style = {styles.Title}>Weather</Text>),
+//             headerRight: (
+//                 <Button title="Change zip" onPress={() => navigation.navigate('ZipCode')}
+//                 />
+//             )
+//         }
+//     }
+//     render() {
+//         const zipCode = this.props.navigation.getParam('zipCode')
+//         return (<Weather zipCode={zipCode} />);
+//     }
+
+// }
+// const styles = StyleSheet.create (
+//     Title: {fontSize : 30 , color : 'red'} , 
+// );
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+import { StyleSheet, Text, View ,Button} from 'react-native';
 import Weather from './Weather';
 
 export default class WeatherScreen extends React.Component {
     static navigationOptions = ({ navigation }) => {
         return {
-            headerTitle: (<Text style = {styles.title}>Weather</Text>),
+            headerTitle: (<Text style={styles.Title}>Weather</Text>),
+            headerStyle: {
+                backgroundColor: '#01579b' ,
+            },
+            
             headerRight: (
-                <Button title="Change zip" onPress={() => navigation.navigate('ZipCode')}
-                />
+                <Button title="Change zip" onPress={() => navigation.navigate('ZipCode')}/>
             )
         }
     }
+
     render() {
         const zipCode = this.props.navigation.getParam('zipCode')
-        return (<Weather zipCode={zipCode} />);
-    }
+        return (
 
+            <Weather zipCode={zipCode} />
+        );
+    }
 }
-const styles = StyleSheet.create (
-    title : {fontSize : 30 , color : 'red'} , 
-);
+
+const styles = StyleSheet.create({
+    Title: { textAlign: 'center', fontSize: 20, color: 'black' },
+});
